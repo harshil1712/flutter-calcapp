36f1405f8801d5f6f2d180125b3c390f39774807
Harshil Agrawal <ghagrawal17@gmail.com>
2018-06-30 18:23:48 +0530

    setup

---
+++ android/app/src/main/java/com/example/calcapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.calcapp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
